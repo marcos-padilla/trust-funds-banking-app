40a2b4a8da5c8e1c1c6ddf3c792ba315f7c07248
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-05-18 19:23:52 -0400

    feat(auth): send unauthenticated users to sign in page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,34 @@
+import { getLoggedInUser } from '@/actions/user-actions'
+import MobileNav from '@/components/mobile-nav'
 import Sidebar from '@/components/sidebar'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	const loggedIn = {
-		firstName: 'John',
-		lastName: 'Doe',
-	}
+	const loggedIn = await getLoggedInUser()
+	if (!loggedIn) return redirect('/sign-in')
 	return (
 		<main className='flex h-screen w-full font-inter'>
-			{/* @ts-ignore*/}
 			<Sidebar user={loggedIn} />
-			{children}
+
+			<div className='flex size-full flex-col'>
+				<div className='root-layout'>
+					<Image
+						src='/icons/logo.svg'
+						width={30}
+						height={30}
+						alt='logo'
+					/>
+					<div>
+						<MobileNav user={loggedIn} />
+					</div>
+				</div>
+				{children}
+			</div>
 		</main>
 	)
 }
---
+++ components/mobile-nav.tsx
@@ -0,0 +1,7 @@
+interface MobileNavProps {
+	user: User
+}
+
+export default function MobileNav({ user }: MobileNavProps) {
+	return <div>MobileNav</div>
+}
